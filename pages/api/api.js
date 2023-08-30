4767b283f07e276207368694836b2f86c7bb3121
ansumancode <dasansuman12@gmail.com>
2023-08-30 08:43:44 +0530

    css fix

--- pages/api/api.js
+++ pages/api/api.js
@@ -13,7 +13,6 @@ export const fetchData = async () => {
 export const submitFormData = async (formData) => {
   try {
     const response = await axios.post("https://64e74133b0fd9648b78fa111.mockapi.io/dataFromStepForm", formData);
-    console.log("Form submitted successfully.");
     return response.data;
   } catch (error) {
     console.error("Error submitting form:", error);
@@ -24,7 +23,6 @@ export const submitFormData = async (formData) => {
 export const updateFormData = async (id, formData) => {
     try {
       const response = await axios.put(`https://64e74133b0fd9648b78fa111.mockapi.io/dataFromStepForm/${id}`, formData);
-      console.log(response, "Form submitted successfully.");
       return response.data;
     } catch (error) {
       console.error("Error submitting form:", error);
@@ -35,10 +33,8 @@ export const updateFormData = async (id, formData) => {
   export const deleteFormData = async (id) => {
     try {
       const response = await axios.delete(`https://64e74133b0fd9648b78fa111.mockapi.io/dataFromStepForm/${id}`);
-      console.log(response, "Form deleted successfully.");
       return response.data;
     } catch (error) {
-      console.error("Error deleting form:", error);
       throw error;
     }
   };
